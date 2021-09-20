087258a30b03eed1c15b7f91d312bead675f6cce
TechPratham-ai <91055688+TechPratham-ai@users.noreply.github.com>
2021-09-20 14:35:41 +0530

    Create Bot.py

---
+++ Bot.py
@@ -0,0 +1,10 @@
+import time
+
+print("Bot :: Hii am Bot from TecPratham")
+time.sleep(2.4)
+print("Bot :: Your Name ? ")
+n=input("You :: ")
+time.sleep(2)
+print("Bot :: nice Name !!")
+time.sleep(1)
+print("Bot :: Nice To meet You
